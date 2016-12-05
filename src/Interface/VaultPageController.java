86c7d6afe06d4177afd435402b053b2d36bec9e8
Fabian Bächli <habian.baechli@gmail.com>
2016-12-05 16:31:16 +0100

    Progress In Vault Page

--- src/Interface/VaultPageController.java
+++ src/Interface/VaultPageController.java
@@ -4,10 +4,9 @@ import com.jfoenix.controls.*;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
-import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.Pane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -15,15 +14,19 @@ import java.util.ResourceBundle;
 /**
  * Created by Fabian on 01.12.16
  */
+
 public class VaultPageController implements Initializable {
 
     @FXML
-    JFXListView<Label> listView;
+    JFXButton newNodeButton;
 
     @FXML
-    private JFXButton newNodeButton;
+    JFXPopup newNodePopUp;
+
     @Override
     public void initialize(URL fxmlFileLocation, ResourceBundle resources) {
-
+        newNodeButton.setOnAction(event -> {
+            newNodePopUp.setVisible(true);
+        });
     }
 }
