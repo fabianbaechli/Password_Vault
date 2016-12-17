31c28ff15bebc70fa724229303d8ab271a1464a8
Fabian Bächli <habian.baechli@gmail.com>
2016-12-17 21:01:49 +0100

    + Nodes Visbile In GUI

--- src/Interface/VaultPageController.java
+++ src/Interface/VaultPageController.java
@@ -6,12 +6,16 @@ import javafx.fxml.*;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
+
+import java.awt.datatransfer.*;
+import java.awt.Toolkit;
 
 /**
  * Created by Fabian on 01.12.16
@@ -25,17 +29,44 @@ public class VaultPageController implements Initializable {
     @FXML
     JFXListView<Label> listView;
 
+    @FXML
+    AnchorPane groundPane;
+
+    @FXML
+    JFXTextField titleField;
+
+    @FXML
+    JFXTextField usernameField;
+
+    @FXML
+    JFXPasswordField passwordField;
+
+    @FXML
+    JFXButton textToClipboard;
+
+    private ArrayList content;
+
     @Override
     public void initialize(URL fxmlFileLocation, ResourceBundle resources) {
-        Manager manager = new Manager();
-        ArrayList content = manager.getContent();
-        System.out.println(content);
-        for (Object aContent : content) {
-            if (aContent.toString().startsWith("title")) {
-                Label label = new Label(aContent.toString());
-                listView.getItems().add(label);
+        groundPane.setOnMouseEntered(event -> {
+
+            listView.getItems().removeAll(listView.getItems());
+            Manager manager = new Manager();
+            content = manager.getContent();
+            System.out.println("refreshed side panel content");
+            for (Object aContent : content) {
+                if (aContent.toString().startsWith("title")) {
+                    Label label = new Label(aContent.toString().replace("title: ", ""));
+                    listView.getItems().add(label);
+
+                    label.setOnMouseClicked(labelClick -> {
+                        drawContent(label);
+                    });
+                }
             }
-        }
+
+        });
+
         newNodeButton.setOnAction(event -> {
             Parent root;
             try {
@@ -49,5 +80,33 @@ public class VaultPageController implements Initializable {
                 e.printStackTrace();
             }
         });
+    }
+
+    private void drawContent(Label titleLabel) {
+        textToClipboard.setVisible(true);
+
+        titleField.setVisible(true);
+        usernameField.setVisible(true);
+        passwordField.setVisible(true);
+        titleField.setText("");
+        usernameField.setText("");
+        passwordField.setText("");
+        for (int i = 0; i < content.size(); i++) {
+            if (content.get(i).toString().replace("title: ", "").equals(titleLabel.getText())) {
+                String titleFieldText = content.get(i).toString().replace("title: ", "");
+                String passwordFieldText = content.get(i - 1).toString().replace("password: ", "");
+                String usernameFieldText = content.get(i - 2).toString().replace("username: ", "");
+
+                titleField.setText(titleFieldText);
+                usernameField.setText(usernameFieldText);
+                passwordField.setText(passwordFieldText);
+                textToClipboard.setOnAction(event -> {
+                    StringSelection stringSelection = new StringSelection(passwordFieldText);
+                    Clipboard textToClipBoard = Toolkit.getDefaultToolkit().getSystemClipboard();
+                    textToClipBoard.setContents(stringSelection, null);
+                });
+                return;
+            }
+        }
     }
 }
